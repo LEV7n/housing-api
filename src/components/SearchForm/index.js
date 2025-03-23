006ad8fd5857985f260ef10876bbf954570a9ffc
Michael Levinez <levinez.michael@gmail.com>
2025-03-23 08:56:17 +0500

    Beautify anything

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,9 +1,30 @@
 import axios from 'axios';
 import './index.css';
 
+/**
+ * @typedef {Object} searchParams
+ * @prop {String} name
+ * @prop {Number|null} bedrooms
+ * @prop {Number|null} bathrooms
+ * @prop {Number|null} storeys
+ * @prop {Number|null} garages
+ * @prop {Number|null} price_min
+ * @prop {Number|null} price_max
+ * @prop {Number} page
+ */
+
+/**
+ * @typedef {Object} results
+ * @prop {Array} data
+ * @prop {Number} total
+ * @prop {Number} per_page
+ * @prop {Number} current_page
+ */
+
 export default {
   data() {
     return {
+      /** @type {searchParams} */
       searchParams: {
         name: '',
         bedrooms: null,
@@ -14,8 +35,11 @@ export default {
         price_max: null,
         page: 1
       },
+
+      /** @type {searchParams|null} */
       lastParams: null,
 
+      /** @type {results} */
       results: {
         data: [],
         total: 0,
@@ -23,11 +47,31 @@ export default {
         current_page: 1
       },
 
+      /** @type {Boolean} */
       loading: false,
+      /** @type {Boolean} */
       noResults: false
     };
   },
   methods: {
+    /**
+     * @method formatPrice
+     * @param {Object} row
+     * @param {Object} column
+     * @param {Number} cellValue
+     * @returns {String}
+     */
+    formatPrice(row, column, cellValue) {
+      const
+        /** @type {NumberFormat} */
+        formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        });
+
+        return formatter.format(cellValue);
+    },
+
     /**
      * @method search
      * @param {SubmitEvent|undefined} e 
@@ -37,18 +81,24 @@ export default {
       this.noResults = false;
 
       if (e) {
-        this.searchParams.page = 1; // Update the page parameter
+        /* Update the page parameter */
+        this.searchParams.page = 1;
         this.lastParams = { ...this.searchParams };
       }
 
-      // On real project we need to separate use ./js/api.js or sdk.js but it here for example
+      /**
+       * On real project we need to separate use ./js/api.js or sdk.js but it here for example
+       * also instead of css use sass, less
+       */
       try {
-        const response = await axios.get('/api/housings/search', {
-          params: this.lastParams,
-        });
+        const
+          /** @type {results} */
+          response = await axios.get('/api/housings/search', {
+            params: this.lastParams
+          });
 
         this.results = response.data;
-        this.noResults = this.results.data.length === 0;
+        this.noResults = !this.results?.data?.length;
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -61,8 +111,11 @@ export default {
      * @param {Number} page
      */
     handlePageChange(page) {
-      this.lastParams.page = page; // Update the page parameter
-      this.search(); // Fetch data for the new page
+      /* Update the page parameter */
+      this.lastParams.page = page;
+
+      /* Fetch data for the new page */
+      this.search();
     }
   }
 };
--- src/components/SearchForm/index.vue
+++ src/components/SearchForm/index.vue
@@ -1,45 +1,67 @@
 <template>
   <div>
+    <!-- Form -->
     <el-form :model="searchParams" @submit.prevent="search">
+      <!-- Name -->
       <el-form-item label="Name">
-        <el-input v-model="searchParams.name" placeholder="Enter name"></el-input>
+        <el-input v-model="searchParams.name" placeholder="Enter name" />
       </el-form-item>
 
+      <!-- Bedrooms -->
       <el-form-item label="Bedrooms">
-        <el-input-number v-model="searchParams.bedrooms" :min="1"></el-input-number>
+        <el-input-number v-model="searchParams.bedrooms" :min="1" />
       </el-form-item>
 
+      <!-- Bathrooms -->
       <el-form-item label="Bathrooms">
-        <el-input-number v-model="searchParams.bathrooms" :min="1"></el-input-number>
+        <el-input-number v-model="searchParams.bathrooms" :min="1" />
       </el-form-item>
 
+      <!-- Storeys -->
       <el-form-item label="Storeys">
-        <el-input-number v-model="searchParams.storeys" :min="1"></el-input-number>
+        <el-input-number v-model="searchParams.storeys" :min="1" />
       </el-form-item>
 
+      <!-- Garages -->
       <el-form-item label="Garages">
-        <el-input-number v-model="searchParams.garages" :min="1"></el-input-number>
+        <el-input-number v-model="searchParams.garages" :min="1" />
       </el-form-item>
 
+      <!-- Price Range -->
       <el-form-item label="Price Range">
-        <el-input-number v-model="searchParams.price_min" placeholder="Min price"></el-input-number>
-        <el-input-number v-model="searchParams.price_max" placeholder="Max price"></el-input-number>
+        <el-input-number v-model="searchParams.price_min" placeholder="Min price" />
+        <el-input-number v-model="searchParams.price_max" placeholder="Max price" />
       </el-form-item>
 
+      <!-- Search -->
       <el-form-item>
-        <el-button type="primary" :loading="loading" @click="search">Search</el-button>
+        <el-button
+          type="primary"
+          :loading="loading"
+          @click="search"
+        >Search</el-button>
       </el-form-item>
     </el-form>
 
-    <el-table :data="results.data" v-loading="loading" style="width: 100%">
-      <el-table-column prop="name" label="Name"></el-table-column>
-      <el-table-column prop="price" label="Price"></el-table-column>
-      <el-table-column prop="bedrooms" label="Bedrooms"></el-table-column>
-      <el-table-column prop="bathrooms" label="Bathrooms"></el-table-column>
-      <el-table-column prop="storeys" label="Storeys"></el-table-column>
-      <el-table-column prop="garages" label="Garages"></el-table-column>
+    <!-- Table -->
+    <el-table
+      :data="results.data"
+      v-loading="loading"
+      style="width: 100%"
+    >
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="bedrooms" label="Bedrooms" />
+      <el-table-column prop="bathrooms" label="Bathrooms" />
+      <el-table-column prop="storeys" label="Storeys" />
+      <el-table-column prop="garages" label="Garages" />
+      <el-table-column
+        prop="price"
+        label="Price"
+        :formatter="formatPrice"
+      />
     </el-table>
 
+    <!-- Pagination -->
     <el-pagination
       v-if="results?.data?.length"
       background
@@ -48,9 +70,15 @@
       :page-size="results.per_page"
       :current-page="results.current_page"
       @current-change="handlePageChange"
-    ></el-pagination>
+    />
 
-    <el-alert v-if="noResults" type="info" title="No results found." show-icon></el-alert>
+    <!-- Alert -->
+    <el-alert
+      v-if="noResults"
+      type="info"
+      title="No results found."
+      show-icon
+    />
   </div>
 </template>
 
